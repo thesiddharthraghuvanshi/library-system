4cc7cff913a025aba8423625690ce97c73f15bce
Siddharth Raghuvanshi <thesiddharthraghuvanshi@gmail.com>
2024-07-23 23:17:48 +0530

    Adding Documentation & ReadME

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# library-system
+# Library System
 RESTful API that manages a Library System
+
+## RUN
+    locate LibrarySystemApplication and run the class
+## Swagger
+Use below link to open swagger html
+    http://localhost:8080/swagger-ui/index.html
+
+### Asumptions
+    1- ISBN number is a string (alpah-numeric)
+
+    2- different app.props file can be find "./resource/..."
+
+    3- used springdoc-openapi-starter-webmvc-ui for documentation
+
+    4- SecurityConfigs file can be find "./common/config/..."
+
+### Database - MySQL
+    1- Open Source and Cost-Effective: 
+        MySQL is open-source software, which means it's free to use for most applications. 
+        This can significantly reduce licensing costs compared to proprietary databases.
+
+    2- ACID Compliance: 
+        MySQL ensures ACID (Atomicity, Consistency, Isolation, Durability) compliance for transactions, 
+        which ensures data integrity and reliability.
+
+    3- Performance: 
+        MySQL is known for its speed and performance. 
+        It can handle a large number of concurrent connections and transactions efficiently, which is crucial for scalable applications.
+
+    4- Scalability: 
+        MySQL can scale both vertically (by adding more resources to a single server) and 
+        horizontally (by distributing data across multiple servers). This scalability makes it suitable for growing applications.
--- src/main/java/com/sid/library/controller/BookController.java
+++ src/main/java/com/sid/library/controller/BookController.java
@@ -10,8 +10,15 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("book")
+@Tag(name = "Book Controller", description = "Endpoints for managing books")
 @Slf4j
 public class BookController {
 
@@ -19,12 +26,22 @@ public class BookController {
     BookService bookService;
 
     @PostMapping("register")
+    @Operation(summary = "Register a new book")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully registered the book"),
+            @ApiResponse(responseCode = "400", description = "Invalid request body")
+    })
     public ResponseEntity<String> registerBook(@RequestBody BookDto bookDto) {
         log.info("Inside registerBook | BookController");
         return bookService.registerBook(bookDto);
     }
 
     @GetMapping("getAll")
+    @Operation(summary = "Get all books")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved the books"),
+            @ApiResponse(responseCode = "404", description = "No books found")
+    })
     public ResponseEntity<List<BookEto>> getAllBooks() {
         log.info("Inside getAllBooks | BookController");
         return bookService.getAllBooks();
